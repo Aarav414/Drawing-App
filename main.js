4e73e3acd407e90ffba66ee03a779999fff44599
Aarav414 <84316227+Aarav414@users.noreply.github.com>
2021-08-30 19:21:19 +1000

    Add files via upload

--- main.js
+++ main.js
@@ -1,33 +1,26 @@
-var mousevent = "empty"
+var mouseEvent = "empty";
 var last_position_of_x, last_position_of_y;
-
-canvas = document.getElementById("myCanvas");
-
+canvas = document.getElementById('myCanvas');
 ctx = canvas.getContext("2d");
-
-var color = "black";
-var width = 1;
-
+color = "black";
+width_of_line = 1;
 canvas.addEventListener("mousedown", my_mousedown);
 
 function my_mousedown(e) {
     color = document.getElementById("color").value;
-    width = document.getElementById("width").value;
-    mousevent = "mousedown";
-
+    width_of_line = document.getElementById("width").value;
+    mouseEvent = "mouseDown";
 }
 canvas.addEventListener("mousemove", my_mousemove);
 
 function my_mousemove(e) {
     current_position_of_mouse_x = e.clientX - canvas.offsetLeft;
     current_position_of_mouse_y = e.clientY - canvas.offsetTop;
-    if (mousevent == "mousedown")
-        
-     {
+    if (mouseEvent == "mouseDown") {
         ctx.beginPath();
         ctx.strokeStyle = color;
-        ctx.lineWidth = width;
-        console.log("last postion of x and y coordinates= ")
+        ctx.lineWidth = width_of_line;
+        console.log("Last position of x and y coordinates = ");
         console.log("x = " + last_position_of_x + "y = " + last_position_of_y);
         ctx.moveTo(last_position_of_x, last_position_of_y);
         console.log("Current position of x and y coordinates = ");
@@ -41,12 +34,12 @@ function my_mousemove(e) {
 canvas.addEventListener("mouseup", my_mouseup);
 
 function my_mouseup(e) {
-    mouseevent = "mouseUP";
+    mouseEvent = "mouseUP";
 }
 canvas.addEventListener("mouseleave", my_mouseleave);
 
 function my_mouseleave(e) {
-    mouseevent = "mouseleave";
+    mouseEvent = "mouseleave";
 }
 
 function clearArea() {
